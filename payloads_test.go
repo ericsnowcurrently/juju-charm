1d156303a3d4ee13e47a923635925d4aa82d130a
Eric Snow <ericsnowcurrently@gmail.com>
2015-10-19 10:05:35 -0600

    Add value checks to Payload.Validate().

--- payloads_test.go
+++ payloads_test.go
@@ -4,6 +4,8 @@
 package charm_test
 
 import (
+	"fmt"
+
 	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
 
@@ -60,6 +62,18 @@ func (s *payloadClassSuite) TestValidateFull(c *gc.C) {
 	c.Check(err, jc.ErrorIsNil)
 }
 
+func (s *payloadClassSuite) TestValidateValidName(c *gc.C) {
+	for _, name := range []string{
+		"mypayload",
+		"my-payload",
+		"my_payload",
+		"my0-payload",
+		"my-payload0",
+	} {
+		checkPayloadName(c, name, "")
+	}
+}
+
 func (s *payloadClassSuite) TestValidateZeroValue(c *gc.C) {
 	var payloadClass charm.PayloadClass
 	err := payloadClass.Validate()
@@ -76,6 +90,37 @@ func (s *payloadClassSuite) TestValidateMissingName(c *gc.C) {
 	c.Check(err, gc.ErrorMatches, `payload class missing name`)
 }
 
+const (
+	punctuation       = `!"#$%&'()*+,-./:;<=>?@[\]^_` + "`" + `{|}~`
+	payloadInvalidMsg = `payload class name .* not valid`
+)
+
+func (s *payloadClassSuite) TestValidateInvalidNameStart(c *gc.C) {
+	msg := payloadInvalidMsg
+	for _, chr := range []byte(punctuation + `0123456789`) {
+		checkPayloadName(c, fmt.Sprintf("%cmy-payload", chr), msg)
+	}
+}
+
+func (s *payloadClassSuite) TestValidateInvalidNameMiddle(c *gc.C) {
+	msg := payloadInvalidMsg
+	for _, chr := range []byte(punctuation) {
+		if chr == '-' || chr == '_' {
+			continue
+		}
+		checkPayloadName(c, fmt.Sprintf("my%cpayload", chr), msg)
+	}
+}
+
+func (s *payloadClassSuite) TestValidateInvalidNameEnd(c *gc.C) {
+	msg := payloadInvalidMsg
+	for _, chr := range []byte(punctuation) {
+		checkPayloadName(c, fmt.Sprintf("my-payload%c", chr), msg)
+	}
+	checkPayloadName(c, "my-payload-", msg)
+	checkPayloadName(c, "my-payload_", msg)
+}
+
 func (s *payloadClassSuite) TestValidateMissingType(c *gc.C) {
 	payloadClass := charm.PayloadClass{
 		Name: "my-payload",
@@ -83,4 +128,20 @@ func (s *payloadClassSuite) TestValidateMissingType(c *gc.C) {
 	err := payloadClass.Validate()
 
 	c.Check(err, gc.ErrorMatches, `payload class missing type`)
+}
+
+func checkPayloadName(c *gc.C, name, msg string) {
+	c.Logf("checking payload name %q", name)
+
+	payloadClass := charm.PayloadClass{
+		Name: name,
+		Type: "docker",
+	}
+	err := payloadClass.Validate()
+
+	if msg == "" {
+		c.Check(err, jc.ErrorIsNil)
+	} else {
+		c.Check(err, gc.ErrorMatches, msg)
+	}
 }
